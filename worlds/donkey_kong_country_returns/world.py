85953ada1887ff6b036b80c99abb86c50de3ff50
Zero <120384602+CallmeZewo@users.noreply.github.com>
2026-01-18 11:32:15 +0100

    Create world.py
    Finallizing world and generationg worked fine

---
+++ worlds/donkey_kong_country_returns/world.py
@@ -0,0 +1,46 @@
+from collections.abc import Mapping
+from typing import Any
+
+from worlds.AutoWorld import World
+
+from . import items, locations, regions, rules, web_world
+from . import options as dkcr_options
+
+class DKCR(World):
+    """
+    Donkey Kong Country Returns is a Platform Collector
+    Defeat the Tikis with your Pal Diddy Kong
+    """
+
+    game = "Donkey Kong Country Returns"
+
+    web = web_world.DKCRWebWorld()
+
+    options_dataclass = dkcr_options.DKCROptions
+    options: dkcr_options.DKCROptions
+
+    location_name_to_id = locations.LOCATEN_NAME_TO_ID
+    item_name_to_id = items.ITEM_NAME_TO_ID
+
+    origin_region_name = "Jungle"
+
+    def create_regions(self) -> None:
+        regions.create_and_connect_regions(self)
+        locations.create_all_locations(self)
+
+    def set_rules(self) -> None:
+        rules.set_all_rules(self)
+
+    def create_items(self) -> None:
+        items.create_all_items(self)
+
+    def create_item(self, name: str) -> items.DKCRItem:
+        return items.create_item_with_correct_classification(self, name)
+
+    def get_filler_item_name(self) -> str:
+        return items.get_random_filler_item_name(self)
+
+    def fill_slot_data(self) -> Mapping[str, Any]:
+        return self.options.as_dict(
+            "golden_temple"
+        )
