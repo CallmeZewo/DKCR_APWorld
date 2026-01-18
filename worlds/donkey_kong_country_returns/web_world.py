a14d615a8d2660976ec535d7eafc202550b8385c
Zero <120384602+CallmeZewo@users.noreply.github.com>
2026-01-18 11:31:52 +0100

    Create web_world.py

---
+++ worlds/donkey_kong_country_returns/web_world.py
@@ -0,0 +1,23 @@
+from BaseClasses import Tutorial
+from worlds.AutoWorld import WebWorld
+
+from .options import option_groups, option_presets
+
+class DKCRWebWorld(WebWorld):
+    game = "Donkey Kong Country Returns"
+
+    theme = "jungle"
+
+    setup_en = Tutorial(
+        "Multiworld Setup Guide",
+        "A guide to setting up DKCR for MultiWorld.",
+        "English",
+        "en_Donkey Kong Country Returns.md",
+        "setup/en",
+        ["CallmeZero"],
+    )
+
+    tutorials = [setup_en]
+
+    option_groups = option_groups
+    option_presets = option_presets
