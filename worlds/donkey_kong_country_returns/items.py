f41e3cca0a2b9cc2763c03852b65286d1c0f7a5e
Zero <120384602+CallmeZewo@users.noreply.github.com>
2026-01-22 19:50:54 +0100

    Update items.py
    Making own ItemData class
    Exchanged values in item_table
    made the itempool via code

--- worlds/donkey_kong_country_returns/items.py
+++ worlds/donkey_kong_country_returns/items.py
@@ -1,61 +1,59 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, NamedTuple, Optional, Dict
+from typing import TYPE_CHECKING, Dict
 
 from BaseClasses import Item, ItemClassification as IC
 
-from strings import Item as I
+from .strings import Item as I
 
 if TYPE_CHECKING:
     from .world import DKCRWorld
 
-class ItemData(NamedTuple):
-    code: Optional[int]
-    classification: IC
-    amount: Optional[int] = 1
+class ItemData:
+    def __init__(self, code, classification: IC = IC.progression, amount=1):
+        self.code = code
+        self.classification = classification
+        self.amount = amount
 
 item_table: Dict[str, ItemData] = {
-    I.PUZZLE_PIECE: ItemData(1, IC.progression, 366),
-    I.Kong_Letter.KONG_LETTER_JUNGLE: ItemData(2, IC.progression, 24),
-    I.Kong_Letter.KONG_LETTER_BEACH: ItemData(3, IC.progression, 28),
-    I.Kong_Letter.KONG_LETTER_RUINS: ItemData(4, IC.progression, 24),
-    I.Kong_Letter.KONG_LETTER_CAVE: ItemData(5, IC.progression, 20),
-    I.Kong_Letter.KONG_LETTER_FOREST: ItemData(6, IC.progression, 32),
-    I.Kong_Letter.KONG_LETTER_CLIFF: ItemData(7, IC.progression, 32),
-    I.Kong_Letter.KONG_LETTER_FACTORY: ItemData(8, IC.progression, 28),
-    I.Kong_Letter.KONG_LETTER_VOLCANO: ItemData(9, IC.progression, 28),
-    I.Key.JUNGLE_KEY: ItemData(10, IC.progression),
-    I.Key.Beach_KEY: ItemData(11, IC.progression),
-    I.Key.Ruins_KEY: ItemData(12, IC.progression),
-    I.Key.CAVE_KEY: ItemData(13, IC.progression),
-    I.Key.FOREST_KEY: ItemData(14, IC.progression),
-    I.Key.CLIFF_KEY: ItemData(15, IC.progression),
-    I.Key.FACTORY_KEY: ItemData(16, IC.progression),
-    I.Key.VOLCANO_KEY: ItemData(17, IC.progression),
-    I.Rare_Orb.GREEN_ORB_JUNGLE: ItemData(18, IC.progression),
-    I.Rare_Orb.BLUE_ORB_BEACH: ItemData(19, IC.progression),
-    I.Rare_Orb.WHITE_ORB_RUINS: ItemData(20, IC.progression),
-    I.Rare_Orb.MAGENTA_ORB_CAVE: ItemData(21, IC.progression),
-    I.Rare_Orb.YELLOW_ORB_FOREST: ItemData(22, IC.progression),
-    I.Rare_Orb.ORANGE_ORB_CLIFF: ItemData(23, IC.progression),
-    I.Rare_Orb.GRAY_ORB_FACTORY: ItemData(24, IC.progression),
-    I.Rare_Orb.RED_ORB_VOLCANO: ItemData(25, IC.progression),
-    I.Shop.BALLOONX1: ItemData(26, IC.useful, 8),
-    I.Shop.BALLOONX3: ItemData(27, IC.useful, 8),
-    I.Shop.BALLOONX7: ItemData(28, IC.useful, 8),
-    I.Shop.SQUAWKS: ItemData(29, IC.useful, 8),
-    I.Shop.HEART_BOOST: ItemData(30, IC.useful, 8),
-    I.Shop.BANANA_JUICE: ItemData(31, IC.useful, 8),
-    I.Unlockables.Moves.RUN: ItemData(32, IC.progression),
-    I.Unlockables.Moves.ROLL: ItemData(33, IC.progression),
-    I.Unlockables.Moves.GRAB: ItemData(34, IC.progression),
-    I.Unlockables.Moves.BLOW: ItemData(35, IC.progression),
-    I.Unlockables.Moves.GROUND_POUND: ItemData(36, IC.progression),
-    I.Unlockables.MISC.ROCKET_BARREL_FUEL: ItemData(37, IC.progression),
-    I.Unlockables.MISC.MINECART_PASS: ItemData(38, IC.progression),
-    I.Unlockables.MISC.RAMBIS_SADDLE: ItemData(39, IC.progression),
-    I.Unlockables.MISC.KONG_BARREL: ItemData(40, IC.progression),
-    I.Unlockables.MIRROR_SHARD: ItemData(41, IC.progression)
+    I.PUZZLE_PIECE: ItemData(code=1, amount=366),
+    I.Kong_Letter.KONG_LETTER_JUNGLE: ItemData(code=2, amount=24),
+    I.Kong_Letter.KONG_LETTER_BEACH: ItemData(code=3, amount=28),
+    I.Kong_Letter.KONG_LETTER_RUINS: ItemData(code=4, amount=24),
+    I.Kong_Letter.KONG_LETTER_CAVE: ItemData(code=5, amount=20),
+    I.Kong_Letter.KONG_LETTER_FOREST: ItemData(code=6, amount=32),
+    I.Kong_Letter.KONG_LETTER_CLIFF: ItemData(code=7, amount=32),
+    I.Kong_Letter.KONG_LETTER_FACTORY: ItemData(code=8, amount=28),
+    I.Kong_Letter.KONG_LETTER_VOLCANO: ItemData(code=9, amount=28),
+    I.Key.JUNGLE_KEY: ItemData(code=10),
+    I.Key.Beach_KEY: ItemData(code=11),
+    I.Key.Ruins_KEY: ItemData(code=12),
+    I.Key.CAVE_KEY: ItemData(code=13),
+    I.Key.FOREST_KEY: ItemData(code=14),
+    I.Key.CLIFF_KEY: ItemData(code=15),
+    I.Key.FACTORY_KEY: ItemData(code=16),
+    I.Key.VOLCANO_KEY: ItemData(code=17),
+    I.Rare_Orb.GREEN_ORB_JUNGLE: ItemData(code=18),
+    I.Rare_Orb.BLUE_ORB_BEACH: ItemData(code=19),
+    I.Rare_Orb.WHITE_ORB_RUINS: ItemData(code=20),
+    I.Rare_Orb.MAGENTA_ORB_CAVE: ItemData(code=21),
+    I.Rare_Orb.YELLOW_ORB_FOREST: ItemData(code=22),
+    I.Rare_Orb.ORANGE_ORB_CLIFF: ItemData(code=23),
+    I.Rare_Orb.GRAY_ORB_FACTORY: ItemData(code=24),
+    I.Rare_Orb.RED_ORB_VOLCANO: ItemData(code=25),
+    I.Shop.BALLOONX1: ItemData(code=26, classification=IC.filler, amount=8),
+    I.Shop.BALLOONX3: ItemData(code=27, classification=IC.filler, amount=8),
+    I.Shop.BALLOONX7: ItemData(code=28, classification=IC.filler, amount=8),
+    I.Unlockables.Moves.RUN: ItemData(code=29),
+    I.Unlockables.Moves.ROLL: ItemData(code=30),
+    I.Unlockables.Moves.GRAB: ItemData(code=31),
+    I.Unlockables.Moves.BLOW: ItemData(code=32),
+    I.Unlockables.Moves.GROUND_POUND: ItemData(code=33),
+    I.Unlockables.MISC.ROCKET_BARREL_FUEL: ItemData(code=34),
+    I.Unlockables.MISC.MINECART_PASS: ItemData(code=34),
+    I.Unlockables.MISC.RAMBIS_SADDLE: ItemData(code=35),
+    I.Unlockables.MISC.KONG_BARREL: ItemData(code=37),
+    I.Unlockables.MIRROR_SHARD: ItemData(code=38),
 }
 
 ITEM_NAME_TO_ID: Dict[str, int] = {}
@@ -68,34 +66,19 @@ class DKCRItem(Item):
     game = "Donkey Kong Country Returns"
 
 def get_random_filler_item_name(world: DKCRWorld) -> str:
-    return "Balloon"
+    return I.Shop.BALLOONX1
 
 def create_item_with_correct_classification(world: DKCRWorld, name: str) -> DKCRItem:
-    classification = DEFAULT_ITEM_CLASSIFICATIONS[name]
+    classification = item_table[name].classification
+    id = item_table[name].code
 
-    return DKCRItem(name, classification, ITEM_NAME_TO_ID[name], world.player)
+    return DKCRItem(name, classification, id, world.player)
 
 def create_all_items(world: DKCRWorld) -> None:
-    itempool: list[Item] = [
-        world.create_item("Puzzle Piece"),
-        world.create_item("Kong Letter"),
-        world.create_item("Jungle Shop Key"),
-        world.create_item("Beach Shop Key"),
-        world.create_item("Ruins Shop Key"),
-        world.create_item("Cave Shop Key"),
-        world.create_item("Forest Shop Key"),
-        world.create_item("Cliff Shop Key"),
-        world.create_item("Factory Shop Key"),
-        world.create_item("Volcano Shop Key"),
-        world.create_item("Defeated Boss 1"),
-        world.create_item("Defeated Boss 2"),
-        world.create_item("Defeated Boss 3"),
-        world.create_item("Defeated Boss 4"),
-        world.create_item("Defeated Boss 5"),
-        world.create_item("Defeated Boss 6"),
-        world.create_item("Defeated Boss 7"),
-        world.create_item("Defeated Boss 8"),
-    ]
+    itempool: list[Item] = []
+    for item in item_table:
+        for _ in range(item_table[item].amount):
+            itempool.append(world.create_item(item))
 
     number_of_Items = len(itempool)
 
