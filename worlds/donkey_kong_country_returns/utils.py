06f01124e2cfac4976a0a9b3c086b0a4d96691ee
Zero <120384602+CallmeZewo@users.noreply.github.com>
2026-06-26 19:26:44 +0200

    added color and game state to utils

--- worlds/donkey_kong_country_returns/utils.py
+++ worlds/donkey_kong_country_returns/utils.py
@@ -2,11 +2,22 @@ import dataclasses
 import dolphin_memory_engine as dme
 from .data.addresses import *
 
+class Color:
+    GREEN = "\033[92m"
+    RED = "\033[91m"
+    YELLOW = "\033[93m"
+    RESET = "\033[0m"
+
 @dataclasses.dataclass()
 class LevelMemData:
     flags: bytes
     record_time: float
     medal: int
+
+def get_game_state():
+    state = int.from_bytes(dme.read_bytes(MEM + GAME_STATE, 4), "big")
+    return state
+
 
 def get_level_data(level_offset: int, ):
     main = int.from_bytes(dme.read_bytes(LEVEL_DATA_POINTER, 4), "big")
